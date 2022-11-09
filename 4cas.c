d2aec8a90337db063f0fca019531e82c1ba3aca4
UBaksa <ujkanovicbakir03@gmail.com>
2022-11-09 23:15:20 +0100

    4cas.c

---
+++ 4cas.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+// main(){
+//     int i,semafor=0;
+//     for(i=1;i<=400;i++){
+//         if(i%4==0&&i%3==0&&i%5==0&&i%7==0){
+//             printf("%d",i);
+//             semafor=1;//nasli smo uslov semafor ide na 1.
+//             break;
+//         }
+//     }
+//     if(semafor==0){
+//         printf("Nema broja");
+//     }
+//     return 0;
+// }
+
+main(){
+    
+}
