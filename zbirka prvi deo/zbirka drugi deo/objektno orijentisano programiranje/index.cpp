87198cb214646234c341a373e1fb78e314abf73d
UBaksa <ujkanovicbakir03@gmail.com>
2023-03-01 09:13:30 +0100

    c++ se grinda sad

---
+++ zbirka prvi deo/zbirka drugi deo/objektno orijentisano programiranje/index.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <math.h>
+ using namespace std;
+//  da se ne pise stalno std ispred samo se include-a.
+
+main(){
+    // da se pogledaju binarni operatori u c-u.
+    // cout<<"Hello World"<<"Zz";
+    // Program koji racuna zbir dva uneta cela broja;
+    // float a,b,c,D1,x1,x2;
+    // cout<<"Unesite dva cela broja: ";
+    // cin>>a>>b;
+    // cout<<"Zbir dva broja "<<a<<" i "<<b<<" iznosi "<<(a+b)<<"\n";
+    // for(int i=0;i<10;i++);//novina da se u for npr moze definisati i sa int ne more na vrh
+    // prosiriti primer da racuna i ispisuje vrednosti armithetickih operacija.
+    //+,-,puta,podeljeno,moduo;
+    // cout<<"Zbir dva broja "<<a<<" i "<<b<<" iznosi "<<(a+b)<<"\n";
+    // cout<<"Razlika dva broja "<<a<<" i "<<b<<" iznosi "<<(a-b)<<"\n";
+    // cout<<"Proizvod dva broja "<<a<<" i "<<b<<" iznosi "<<(a*b)<<"\n";
+    // cout<<"Moduo dva broja "<<a<<" i "<<b<<" iznosi "<<(a%b)<<"\n";
+    // cout<<"Kolicnik dva broja "<<a<<" i "<<b<<" iznosi "<<(a/b)<<"\n";
+    // Izracunati ax2+bx+c=0;
+    // D=koren iz b na kvadrat plus 4ac kroz 2*a;
+    // cout<<"Unesite a,b,c";
+    // cin>>a>>b>>c;
+    // D1=pow(b,2)-4*a*c;
+    // if(D1<0){
+    //     cout<<"Nema realno resenje";
+    // }
+    // else if(D1==0){
+    //     x1=-b/2*a;
+    // cout<<"Ima jedinstveno resenje i ono iznosi x1="<<x1<<"\n";
+    // }
+    // else
+    // {
+    //     x1=(-b+sqrt(D1))/2*a;
+    //     x1=(-b-sqrt(D1))/2*a;
+    // cout<<"x1="<<x1<<"\n";
+    // cout<<"x2="<<x1<<"\n";
+    // }
+    int mesec,godina;
+    cin>>mesec>>godina;
+    switch(godina){
+        case if(godina / 4 == 0 && godina / 100 !=0):
+        switch(mesec){
+            case 1;
+            cout>>"31";
+            break;
+            case 2;
+            cout>>"29";
+            break;
+            case 3;
+            cout>>"31";
+            break;
+            case 4;
+            cout>>"30";
+            break;
+            case 5;
+            cout>>"31";
+            break;
+            case 6;
+            cout>>"30";
+            break;
+            case 7;
+            cout>>"31";
+            break;
+            case 8;
+            cout>>"31";
+            break;
+            case 9;
+            cout>>"30";
+            break;
+            case 10;
+            cout>>"31";
+            break;
+            case 11;
+            cout>>"30";
+            break;
+            case 12;
+            cout>>"31";
+            break;
+        } 
+    }
+    return 0;
+}
