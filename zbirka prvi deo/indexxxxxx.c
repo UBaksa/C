4d4c76fdf9ecd3eee568ecf22eb3b79641901470
UBaksa <ujkanovicbakir03@gmail.com>
2023-03-02 19:41:12 +0100

    c++

---
+++ zbirka prvi deo/indexxxxxx.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+main(){
+    a,b,c;
+    c=b;
+    123456;
+    broj,pom,cifra,s1,s2,n,br;
+    broj scanf...
+    pom=broj;
+    while(pom>0){
+        cifra=cifra+pom%10;
+        pom/=10;
+        br++;
+        if(br<=3){
+            s2+=cifra;
+        }
+        else{
+            s1+=cifra;
+        }
+        if(n%2==0){
+        }
+        if(s1==s2){
+            savrsen;
+        }
+    }
+}
--- zbirka prvi deo/zbirka drugi deo/objektno orijentisano programiranje/index.cpp
+++ zbirka prvi deo/zbirka drugi deo/objektno orijentisano programiranje/index.cpp
@@ -30,56 +30,60 @@ main(){
     // else if(D1==0){
     //     x1=-b/2*a;
     // cout<<"Ima jedinstveno resenje i ono iznosi x1="<<x1<<"\n";
+    // // }
+    // // else
+    // // {
+    // //     x1=(-b+sqrt(D1))/2*a;
+    // //     x1=(-b-sqrt(D1))/2*a;
+    // // cout<<"x1="<<x1<<"\n";
+    // // cout<<"x2="<<x1<<"\n";
+    // // }
+    // int mesec,godina;
+    // cin>>mesec>>godina;
+    // switch(godina){
+    //     case if(godina / 4 == 0 && godina / 100 !=0):
+    //     switch(mesec){
+    //         case 1;
+    //         cout>>"31";
+    //         break;
+    //         case 2;
+    //         cout>>"29";
+    //         break;
+    //         case 3;
+    //         cout>>"31";
+    //         break;
+    //         case 4;
+    //         cout>>"30";
+    //         break;
+    //         case 5;
+    //         cout>>"31";
+    //         break;
+    //         case 6;
+    //         cout>>"30";
+    //         break;
+    //         case 7;
+    //         cout>>"31";
+    //         break;
+    //         case 8;
+    //         cout>>"31";
+    //         break;
+    //         case 9;
+    //         cout>>"30";
+    //         break;
+    //         case 10;
+    //         cout>>"31";
+    //         break;
+    //         case 11;
+    //         cout>>"30";
+    //         break;
+    //         case 12;
+    //         cout>>"31";
+    //         break;
+    //     } 
     // }
-    // else
-    // {
-    //     x1=(-b+sqrt(D1))/2*a;
-    //     x1=(-b-sqrt(D1))/2*a;
-    // cout<<"x1="<<x1<<"\n";
-    // cout<<"x2="<<x1<<"\n";
-    // }
-    int mesec,godina;
-    cin>>mesec>>godina;
-    switch(godina){
-        case if(godina / 4 == 0 && godina / 100 !=0):
-        switch(mesec){
-            case 1;
-            cout>>"31";
-            break;
-            case 2;
-            cout>>"29";
-            break;
-            case 3;
-            cout>>"31";
-            break;
-            case 4;
-            cout>>"30";
-            break;
-            case 5;
-            cout>>"31";
-            break;
-            case 6;
-            cout>>"30";
-            break;
-            case 7;
-            cout>>"31";
-            break;
-            case 8;
-            cout>>"31";
-            break;
-            case 9;
-            cout>>"30";
-            break;
-            case 10;
-            cout>>"31";
-            break;
-            case 11;
-            cout>>"30";
-            break;
-            case 12;
-            cout>>"31";
-            break;
-        } 
-    }
-    return 0;
+    // return 0;
+    
 }
+
+
+
