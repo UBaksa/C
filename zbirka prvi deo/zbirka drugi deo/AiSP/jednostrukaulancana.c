719555cb665878be1001e6b298a162c3a0a70ec9
UBaksa <ujkanovicbakir03@gmail.com>
2023-05-29 23:44:33 +0200

    aisp

---
+++ zbirka prvi deo/zbirka drugi deo/AiSP/jednostrukaulancana.c
@@ -0,0 +1,190 @@
+// jednolancane liste
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node{
+    int data;
+    struct node *next;
+}NODE;
+NODE *first=NULL,*last=NULL,*temp=NULL,*curr=NULL,*prev=NULL,*next=NULL,*prvi,*kraj,*sredina,*brisi,*prethodnik,*zadnji,*odredjena;
+
+
+int isEmpty(){
+    if(first==NULL)return 1;
+    return 0;
+}
+
+
+
+void create(int el){
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=el;
+    temp->next=NULL;
+    if(isEmpty()){
+        first=temp;
+        last=temp;
+    }
+    else{
+        last->next=temp;
+        last=temp;
+    }
+    
+}
+
+
+
+struct NODE *reverse(NODE *first)
+{   
+    curr=first;
+    
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+
+void display(){
+    temp=first;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+}}
+
+int brojac(){
+    int b=0;
+    temp=first;
+    while(temp!=NULL){
+        b++;
+        temp=temp->next;
+    }
+    return b;
+}
+void naPocetak(int brojPocetak){
+    prvi=(struct node*)malloc(sizeof(struct node));
+        prvi->data=brojPocetak;
+        prvi->next=first;
+        first=prvi;
+}
+void naKraj(int brojKraj){
+    kraj=(struct node*)malloc(sizeof(struct node));
+    kraj->data=brojKraj;
+    // kraj->next=NULL;
+    last->next=kraj;
+    last=kraj;
+}
+void uSredinu(int brojSredina){
+    sredina=(struct node*)malloc(sizeof(struct node));
+    int ukupno=brojac();
+    int ukupno2=ukupno/2;
+   int i;
+    if(ukupno%2!=0){
+        printf("neparan broj cvorova,ne moze u sredinu ");
+    } else{
+        temp=first;
+        for( i=1;i<=ukupno2-1;i++){
+            temp=temp->next;
+        }
+        sredina->data=brojSredina;
+        sredina->next=temp->next;
+        temp->next=sredina;
+    }
+}
+void brisiPrvi(){
+  temp=first;
+  first=first->next;
+  free(temp);
+}
+void brisiSredinu(){
+     int ukupno=brojac();
+     int ukupno2=ukupno/2;
+     sredina=first;
+     prethodnik=first;
+     for(int i=1;i<ukupno2+1;i++){
+         prethodnik=sredina;
+         sredina=sredina->next;
+     }
+     prethodnik->next=sredina->next;
+    
+     free(sredina);
+}
+void brisiPoslednji(){
+    temp=first;
+    zadnji=first;
+    while(zadnji->next!=NULL){
+        temp=zadnji;
+        zadnji=zadnji->next;
+    }
+    temp->next=NULL;
+    zadnji=NULL;
+    free(zadnji);
+}
+void search(int position){
+    odredjena=first;
+    temp=first;
+    for(int i=1;i<position;i++){
+        temp=odredjena;
+        odredjena=odredjena->next;
+    }
+    printf("cvor koji ste trazili ima vrednost %d",odredjena->data);
+}
+void niz(){
+    int i=0;
+    temp=first;
+    int niz[100];
+    while(temp!=NULL){
+        niz[i]=temp->data;
+        temp=temp->next;
+        printf("%d",niz[i]);
+        i++;
+    }
+
+}
+void sortiraj(int num_of_nodes){
+  
+    printf("\n");
+    int br1,pom,br2;
+    for(int br1=num_of_nodes-2;br1>=0;br1--){
+        
+        curr=first;
+        next=first->next;
+        for(int br2=0;br2<=br1;br2++){
+            if(curr->data>next->data){
+                pom=next->data;
+                next->data=curr->data;
+                curr->data=pom;
+            }
+            curr=next;
+            next=next->next;
+        }
+    }
+}
+int main()
+{
+    int ukupno;
+    create(4);
+    create(5);
+    create(6);
+    create(0);
+    create(9);
+    // first=reverse(first);
+    // display();
+  naPocetak(1);
+  
+  naKraj(8);
+//   display();
+  ukupno=brojac();
+//   uSredinu(7);
+//   brisiPrvi();
+//   brisiSredinu();
+//   brisiPoslednji();
+  display();
+ search(4);
+ niz();
+    printf("Ima ukupno %d cvorova",ukupno);
+}*/
--- zbirka prvi deo/zbirka drugi deo/AiSP/liste.c
+++ zbirka prvi deo/zbirka drugi deo/AiSP/liste.c
@@ -176,18 +176,108 @@ struct NODE *reverse(NODE *first)
     return prev;
 }
 
-int main(){
-    int n,i,elem;
-    printf("Unesite koliko cete cvorova da imate");
+// int main(){
+//     int n,i,elem;
+//     printf("Unesite koliko cete cvorova da imate");
+//     scanf("%d",&n);
+//     printf("\nUnesite datu za cvorove!");
+//     for(i=1;i<=n;i++){
+//         printf("unesite datu za cvor\n");
+//         scanf("%d",&elem);
+//         kreiranjejednog(elem);
+//     }
+//     display();
+//     // first = reverse(first);//ovo glavno za reverse u main!
+//     // display();
+//     return 0;
+// }
+// 3. Write a program in C to create a singly linked list of n nodes and count the number of nodes.
+// Test Data :
+// Input the number of nodes : 3
+// Input data for node 1 : 5
+// Input data for node 2 : 6
+// Input data for node 3 : 7
+int brojacanik(){
+    int brojac;
+    temp=first;
+    while(temp!=NULL){
+        brojac++;
+        temp=temp->next;
+    }
+    return brojac;
+}
+// main(){
+//     int i,elem,n;
+//     printf("Unesite broj cvorova koliko zelite\n");
+//     scanf("%d",&n);
+//     printf("Nakon poruke unesite elemente unutar cvorova\n");
+//     for(i=1;i<=n;i++){
+//         printf("Unesite %d cvor",i);
+//         scanf("%d",&elem);
+//         kreiranjejednog(elem);
+//     }
+//     printf("Prikazacemo listu\n");
+//     display();
+//     printf("Prebrojali smo %d cvorova",brojacanik());
+//     return 0;
+// }
+// 4. Write a program in C to insert a new node at the beginning of a Singly Linked List.
+void naPocetak(int brojPocetak){
+    prvi=(struct node*)malloc(sizeof(struct node));
+        prvi->data=brojPocetak;
+        prvi->next=first;
+        first=prvi;   
+}
+// 5. Write a program in C to insert a new node at the end of a Singly Linked List.
+void naKraj(int brojKraj){
+    kraj=(struct node*)malloc(sizeof(struct node));
+    kraj->data=brojKraj;
+    kraj->next=NULL;
+    last->next=kraj;
+    last=kraj;
+}
+// 6. Write a program in C to insert a node in the middle of a Singly Linked List
+void uSredinu(int brojSredina){
+    sredina=(struct node*)malloc(sizeof(struct node));
+    int ukupno=brojacanik();
+    int ukupno2=ukupno/2;
+   int i;
+    if(ukupno%2!=0){
+        printf("neparan broj cvorova,ne moze u sredinu ");
+    } else{
+        temp=first;
+        for( i=1;i<ukupno2-1;i++){
+            temp=temp->next;
+        }
+        sredina->data=brojSredina;
+        sredina->next=temp->next;
+        temp->next=sredina;
+    }
+}
+// 7. Write a program in C to delete the first node of a Singly Linked List.
+void brisiPrvi(){
+    temp=first;
+    first=first->next;
+    free(temp);
+}
+main(){
+    int i,elem,n;
+    printf("Unesite broj cvorova koliko zelite\n");
     scanf("%d",&n);
-    printf("\nUnesite datu za cvorove!");
+    printf("Nakon poruke unesite elemente unutar cvorova\n");
     for(i=1;i<=n;i++){
-        printf("unesite datu za cvor\n");
+        printf("Unesite %d cvor",i);
         scanf("%d",&elem);
         kreiranjejednog(elem);
     }
     display();
-    // first = reverse(first);//ovo glavno za reverse u main!
-    // display();
+    // naPocetak(1);
+    // // printf("\nNakon dodatogelementa:\n");
+    // naKraj(3);
+    // printf("\nNakon dodavanja na kraj!");
+    // uSredinu(25);
+    brisiPrvi();
+    printf("\n");
+    display();
     return 0;
 }
