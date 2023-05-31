62e9068830c3b3006082636b6055618a49a03793
UBaksa <ujkanovicbakir03@gmail.com>
2023-05-31 18:20:53 +0200

    aisp

--- zbirka prvi deo/zbirka drugi deo/AiSP/dvostrukeliste.c
+++ zbirka prvi deo/zbirka drugi deo/AiSP/dvostrukeliste.c
@@ -4,66 +4,79 @@
 #include <math.h>
 
 //osnovno kreiranje dvostruke ulancane liste!
-typedef struct node{
-    int data;
-    struct node *next;
-    struct node *prev;
-}NODE;
-NODE *head=NULL,*temp=NULL;
+struct Node  {
+	int data;
+	struct Node* next;
+	struct Node* prev;
+};
+struct Node* head; 
+//za kreiranje novog dvostrukok cvora gde je x element-data.
+struct Node* GetNewNode(int x) {
+	struct Node* newNode
+		= (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = x;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
+}
+//dodavanje na pocetak-vrh 
+void InsertAtHead(int x) {
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	head->prev = newNode;
+	newNode->next = head; 
+	head = newNode;
+}
+//dodavanje na kraj liste,gde cela lista mora da se obidje i da se doda kad temp bude na kraj null;samo kad se obilazi ili brise se kreira temp;
+void InsertAtTail(int x) {
+	struct Node* temp = head;
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL) {
+		head = newNode;
+		return;
+	}
+	while(temp->next != NULL) temp = temp->next; // Go To last Node
+	temp->next = newNode;
+	newNode->prev = temp;
+}
+//prikazivanje liste
+void Print() {
+	struct Node* temp = head;
+	printf("Forward: ");
+	while(temp != NULL) {
+		printf("%d ",temp->data);
+		temp = temp->next;}
+	printf("\n");
+}
+//reverse print ili display.
+//prvo je obidjemo do kraja liste,pa posle pmocu prev mozemo da obidjemo na pocetak,jer prvi pocetnik ima vrednost NULL za prev.
+void ReversePrint() {
+	struct Node* temp = head;
+	if(temp == NULL) return; 
+	while(temp->next != NULL) {
+		temp = temp->next; }
+		printf("Reverse: ");
+	while(temp != NULL) {
+		printf("%d ",temp->data);
+		temp = temp->prev; }
+	printf("\n");
+}
 
-// 1. Write a program in C to create and display a doubly linked list.
-void display(){
-    temp=head;
-    while(temp){
-        printf("%d ",temp->data);
-        temp=temp->next;
-    }
-}
-NODE* New(int el){
-    NODE*temp=(struct node*)malloc(sizeof(struct node));
-    temp->data=el;
-    temp->next=NULL;
-    temp->prev=NULL;
-    return temp;
-}
-//mora da se kreira u for petlju pa da se doda ispred sa ovom ispod funkcijom!
-void addFront(int el){
-    temp=New(el);
-    if(!head){
-        head=temp;
-    }
-    else {
-        head->prev=temp;
-        temp->next=head;
-        head=temp;
-    }
-}
-// 3. Write a program in C to insert a node at the beginning of a doubly linked list
-void addFront(int el){
-    temp=New(el);
-    if(!head){
-        head=temp;
-    }
-    else {
-        head->prev=temp;
-        temp->next=head;
-        head=temp;
-    }
-}
 main(){
-    int i,n,elem;
-    printf("Unesite koliko cvorova zelite\n");
+    int i,n,el;
+    printf("Unesite broj cvorova koliko zelite da imate");
     scanf("%d",&n);
-    printf("\nIspod unesite vrednosti za cvorove!");
-    printf("\n");
     for(i=1;i<=n;i++){
-        printf("Unesite vrednost za %d cvor",i);
-        scanf("%d",&elem);
-        addFront(elem);
+        printf("Unesite element");
+        scanf("%d",&el);
+        InsertAtHead(el);
     }
-    printf("Unesene vrednosti za cvor,prikaz ispod:\n");
-    display();
-    addFront(32);
-    display();
+    // Print();
+    // InsertAtTail(25);
+    Print();
+    ReversePrint();
     return 0;
 }
